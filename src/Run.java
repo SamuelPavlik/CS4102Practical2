c3475b23d020eb62e0ffef1610fe11d25d8e5614
sp217 <sp217@st-andrews.ac.uk>
2019-04-23 16:37:00 +0100

    processing added

--- src/Run.java
+++ src/Run.java
@@ -1,5 +1,32 @@
-public class Run {
-    public static void main(String[] args) {
+import processing.core.PApplet;
 
+public class Run extends PApplet {
+    private static final int WIDTH = 1500;
+    private static final int HEIGHT = 1000;
+    private static Run instance = null;
+
+    public void setup() {
+        instance = this;
+    }
+
+    public void settings(){
+        size(WIDTH, HEIGHT);
+    }
+
+    public void draw(){
+        //Draw scene
+        background(41, 184, 255) ;
+    }
+
+    public static void main(String[] args){
+        String[] processingArgs = {"Run"};
+        Run runClass = new Run();
+        PApplet.runSketch(processingArgs, runClass);
+    }
+
+    public static Run getInstance() {
+        if (instance == null)
+            instance = new Run();
+        return instance;
     }
 }
